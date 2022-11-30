4c18b7ed865887be65255ee2a22544a696e990fe
Qtec <hjp@qtec.kr>
2022-11-30 18:49:20 +0900

    QR 로또번호 추가 기능 작업

--- Lotto/ViewController/MyNumber/ScannerController.swift
+++ Lotto/ViewController/MyNumber/ScannerController.swift
@@ -69,14 +69,50 @@ extension ScannerController {
 extension ScannerController: ScannerViewDelegate {
     
     //http://m.dhlottery.co.kr/?v=0950q091125283342q091014173537q041723323741q232635384145q1112131732431426094184
+    /*
+     round: 950
+     1    : 09 11 25 28 33 42
+     2    : 09 10 14 17 35 37
+     3    : 04 17 23 32 37 41
+     4    : 23 26 35 38 41 45
+     5    : 11 12 13 17 32 43
+     */
     func completion(in status: ScanStatus) {
         switch status {
-        case .success(let _):
-            break
+        case .success(let url):
+            guard let url = url, !url.isEmpty else {
+                return print("url is empty")
+            }
+            
+            var components = url.components(separatedBy: "q")
+            if let round = components[0].components(separatedBy: "v=").last {
+                print("round:", Int(round) ?? 0)
+                components.removeFirst()
+            }
+            
+            var nums = [[Int]]()
+            for component in components {
+                var value = component
+                
+                var num = [Int]()
+                for _ in 0...5 {
+                    let r = value.index(value.startIndex, offsetBy: 2)
+                    if let no = Int(value[value.startIndex..<r]) {
+                        value = String(value[r..<value.endIndex])
+                        num.append(no)
+                    }
+                }
+                
+                print(num)
+                nums.append(num)
+            }
+            
+            print(nums)
+            //TODO: 스캔 후 뒤로간 다음 등록번호 팝업창 띄우기
         case .fail:
-            break
-        case .stop(let _):
-            break
+            print("fail to capture")
+        case .stop(let status):
+            print("button status:", status)
         }
     }
 }
